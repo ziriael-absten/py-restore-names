4069830e912df4fada7c0e6693bd4c072c8ec190
Ziriael Absten <innli1322@gmail.com>
2024-04-09 15:21:04 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,34 @@
 import pytest
 from app.restore_names import restore_names
 
-# write your tests here
+
+@pytest.fixture(scope="module")
+def prepare_users() -> list:
+    users = [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+    return users
+
+
+def test_first_name_is_none(prepare_users: list) -> None:
+    restore_names(prepare_users)
+    assert (
+        prepare_users[0]["first_name"]
+        == prepare_users[0]["full_name"].split()[0]
+    )
+
+
+def test_first_name_not_in_user(prepare_users: list) -> None:
+    restore_names(prepare_users)
+    assert (
+        prepare_users[1]["first_name"]
+        == prepare_users[1]["full_name"].split()[0]
+    )
